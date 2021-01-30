ce4e100c11594ab51cfc827cb1056a8a8450cf43
Chriss Chris <zenskech@outlook.com>
2021-01-30 17:16:39 +0500

    Update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyCi558QRKCSLKTB95f7k4FeAqOPggxuytI',
+  authDomain: 'login-authentication-9cc70.firebaseapp.com',
+  projectId: 'login-authentication-9cc70',
+  storageBucket: 'login-authentication-9cc70.appspot.com',
+  messagingSenderId: '746908344567',
+  appId: '1:746908344567:web:876be80156122dd4bc33f2',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
